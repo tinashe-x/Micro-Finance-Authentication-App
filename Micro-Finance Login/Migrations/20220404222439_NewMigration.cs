4982a71045790f3f345aeab1fcfd36e54829a3e2
tinas <tinas@TINASHES-PC>
2022-04-10 13:19:26 +0200

    Add project files.

---
+++ Micro-Finance Login/Migrations/20220404222439_NewMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Micro_Finance_Login.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
